38bba213b1c33bb4f4494ab05a450ffede3db5ff
mudable <mudableshilpa@gmail.com>
2021-10-29 21:17:10 +0530

    Added :Uc4 to delete the existing contact

--- src/com/bz/address_book/AddressBook.java
+++ src/com/bz/address_book/AddressBook.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-	/* Used Scanner to take contact details from console or user */
+	/*
+	 * Used Scanner to take contact details from console or user added addContact
+	 * details to add the details used edit contacts method to edit the existing
+	 * contact details. Delete method to delete the contact
+	 */
+	private static final Contact con = null;
+	public static Scanner sc = new Scanner(System.in);
 	static ArrayList<Contact> contact1 = new ArrayList<Contact>();
 
 	static void AddContactDetails() {
@@ -27,14 +33,13 @@ public class AddressBook {
 			System.out.println("Enter the email ");
 			String email = scan.next();
 			Contact contact = new Contact(first_name, last_name, address, city, state, ph_no, email);
+			contact1.add(contact);
 		}
 	}
 
 	public static void editContacts(String name) {
-		Scanner sc = new Scanner(System.in);
 		for (Contact con : contact1) {
 			if (name.equalsIgnoreCase(con.first_name)) {
-
 				System.out.println("entered name found in the contact");
 				System.out.println("Enter the updated first_name ");
 				String first_name = sc.next();
@@ -63,7 +68,51 @@ public class AddressBook {
 		}
 	}
 
+	public static void showContacts() {
+		int i = 1;
+		for (Contact contact : contact1) {
+			System.out.println("Details  of contact number " + i + " is");
+			System.out.println(contact.toString());
+			i++;
+		}
+	}
+
+	public static void deleteContact(String name1) {
+		for (int i = 0; i < contact1.size(); i++) {
+			Contact contact = (Contact) contact1.get(i);
+			if (name1.equals(contact.getFirst_name())) {
+				contact.print();
+				contact1.remove(i);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		AddContactDetails();
+		int i = 1;
+		while (i != 0) {
+			System.out.println("Enter 1.To Add 2.Edit 3.Show Details 4.Delete");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				AddContactDetails();
+				break;
+			case 2:
+				System.out.println("Enter the name to edit");
+				String name = sc.next();
+				editContacts(name);
+				break;
+			case 3:
+				showContacts();
+				break;
+			case 4:
+				System.out.println("Enter the name to delete");
+				String name1 = sc.next();
+				deleteContact(name1);
+				break;
+			default:
+				System.out.println("wrong choice");
+				break;
+			}
+		}
 	}
 }
